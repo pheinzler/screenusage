77aaaa90cb8fb989ead79d9a86a155180adfc489
paddy287 <patrick.heinzler@gmx.de>
2022-04-27 17:12:54 +0200

    buttons created and window title changed

--- src/gui.py
+++ src/gui.py
@@ -11,12 +11,16 @@ class Main(tk.Frame):
         self.create_widgets()
     
     def init(self):
-        self.root.minsize(300, 100)
+        self.root.minsize(500, 100) 
     
     def create_widgets(self):
         self.lable = tk.Label(self.root, text= "Start tracking mouse movement and clicking")
         self.lable.grid(row=0 , column= 0)
+        self.start = tk.Button(self.root, text="Start", fg="green").grid(row=1, column=1)
+        self.create = tk.Button(self.root, text="Create", fg="grey").grid(row=1, column=2)
+        self.stop = tk.Button(self.root, text="Stop", fg="red").grid(row=1, column=3)
 if __name__ == '__main__':
     root = tk.Tk()
+    root.title("Screenusage")
     main = Main(root)
     root.mainloop()
