b52510a3540efadbabe06073f2de03ab76fcbd42
paddy287 <patrick.heinzler@gmx.de>
2022-04-27 17:54:13 +0200

    create button removed

--- src/gui.py
+++ src/gui.py
@@ -11,14 +11,13 @@ class Main(tk.Frame):
         self.create_widgets()
     
     def init(self):
-        self.root.minsize(500, 100) 
+        self.root.minsize(400, 100) 
     
     def create_widgets(self):
         self.lable = tk.Label(self.root, text= "Start tracking mouse movement and clicking")
         self.lable.grid(row=0 , column= 0)
         self.start = tk.Button(self.root, text="Start", fg="green").grid(row=1, column=1)
-        self.create = tk.Button(self.root, text="Create", fg="grey").grid(row=1, column=2)
-        self.stop = tk.Button(self.root, text="Stop", fg="red").grid(row=1, column=3)
+        self.stop = tk.Button(self.root, text="Stop", fg="red").grid(row=1, column=2)
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Screenusage")
