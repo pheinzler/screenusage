569cb260803d787a16a591991d4750236f239780
paddy287 <patrick.heinzler@gmx.de>
2022-05-01 14:39:40 +0200

    colourizing in heatmap format

--- main.py
+++ main.py
@@ -7,21 +7,30 @@ import pickle
 #Constants
 WIDTH = 3072
 HEIGHT = 1920
+
 # Colours
-YELLOW = (255,255,0)
+DARKBLUE = (0 , 0 , 153)
+BRIGHTBLUE = (0 , 204 , 204)
+YELLOW = (255 , 255 , 44)
+BRIGHTORANGE = (249 , 191 , 30)
+DARKORANGE = (239 , 134 , 13)
 RED = (255,0,0)
-GREEN = (34 , 193 , 34)
+DARKRED = (146 , 1 , 1)
+GREEN = (94 , 225 , 133)
+colours = [DARKBLUE, BRIGHTBLUE, GREEN, YELLOW, BRIGHTORANGE, DARKORANGE, RED, DARKRED]
 
 #create new data 
 if len(argv) > 1:
-    #load heatmap
-    heatmap = Image.new('RGB', (WIDTH, HEIGHT), "white")
+    #create heatmap
+    heatmap_move = Image.new('RGB', (WIDTH, HEIGHT), "white")
+    heatmap_click = Image.new('RGB', (WIDTH, HEIGHT), "white")
     move_dic = {"max":0}
     click_dic = {"max":0}
 
 #use old data
 else:
-    heatmap = Image.open("heatmap.png")
+    heatmap_move = Image.open("heatmap_move.png")
+    heatmap_click = Image.open("heatmap_click.png")
     # loading Pickle Data sets
     with open("./data.pkl" , "rb") as data:
         data_arr = pickle.load(data)
@@ -30,25 +39,56 @@ else:
         move_dic = data_arr[0]
         click_dic = data_arr[1]
 
-def colourize_more_pixels(x , y , colour , square = 3):
-    if(x > WIDTH - square or y > HEIGHT - square):
-        heatmap.putpixel((x,y) , colour)
-    else:
-        for i in range(x , x + square):
-            for j in range(y , y + square):
-                heatmap.putpixel((i,j) , colour)
+def get_colour(coordinate, difference, dic):
+    colour = None
+    if dic == "move":
+        val = move_dic[coordinate]
+        min = move_dic["min"]
+    elif dic == "click":
+        val = click_dic[coordinate]
+        min = click_dic["min"]
+    one_part = difference / len(colours)
+    for i in range (len(colours)):
+        if i * one_part + min >= val:
+            colour = colours[i]
+            break
+    if colour is None:
+        colour = colours[len(colours) - 1]
+    return colour
+#colourize not just one pixel but square with more pixels
+def colourize_more_pixels(x , y , colour , square = 5, image = None):
+    if image == "move":
+        if(x > WIDTH - square or y > HEIGHT - square):
+            heatmap_move.putpixel((x,y) , colour)
+        else:
+            for i in range(x , x + square):
+                for j in range(y , y + square):
+                    heatmap_move.putpixel((i,j) , colour)
+    elif image == "click":
+        if(x > WIDTH - square or y > HEIGHT - square):
+            heatmap_click.putpixel((x,y) , colour)
+        else:
+            for i in range(x , x + square):
+                for j in range(y , y + square):
+                    heatmap_click.putpixel((i,j) , colour)
 
-def create_image():
+def create_images():
+    #get max and min values as well as differences for colour of pixel
+    click_max , click_min , move_max , move_min = click_dic["max"] , click_dic["min"] , move_dic["max"] , move_dic["min"]
+    move_difference = move_max - move_min
+    click_difference = click_max - click_min
     for coordinate in move_dic.keys():
         if coordinate != "max" and coordinate != "min":
+            colour = get_colour(coordinate , move_difference, "move")
             x = coordinate[0]
             y = coordinate[1]
-            colourize_more_pixels(x,y, GREEN)
+            colourize_more_pixels(x,y, colour, image="move")
     for coordinate in click_dic.keys():
         if coordinate != "max" and coordinate != "min":
+            colour = get_colour(coordinate , click_difference, "click")
             x = coordinate[0]
             y = coordinate[1]
-            colourize_more_pixels(x,y, RED)
+            colourize_more_pixels(x,y, colour, image="click")
 
 #mouse movement
 def on_move(x, y):
@@ -74,37 +114,36 @@ def on_click(x, y, button, pressed):
 #ending service rn
 def on_scroll(x, y, dx, dy):
     listener.stop()
+    #get min values
     click_dic["min"] = click_dic["max"]
     move_dic["min"] = move_dic["max"]
     for ele in click_dic.values():
         if ele < click_dic["min"]:
-            click_dic["min"] = ele
-    
+            click_dic["min"] = ele    
     for ele in move_dic.values():
         if ele < move_dic["min"]:
             move_dic["min"] = ele
-    print("Move dict: \n" , move_dic, "\n\n")
-    print("Click dict: \n" , click_dic)
-
+    print("Creating Images...")
     #colourize pixels in image and safe data
-    create_image()
+    create_images()
+    print("Images created")
+    print("Store data")
     if len(argv) > 1:
-    #store data
+        #store data
         with open("./data_new.pkl", "wb") as data:
             data_to_store = [move_dic , click_dic]
             pickle.dump(data_to_store , data)
             data.close()
-            heatmap.save("heatmap_new.png")
-            
+            heatmap_click.save("heatmap_click_new.png")
+            heatmap_move.save("heatmap_move_new.png")          
     else:
         #store data
         with open("./data.pkl", "wb") as data:
             data_to_store = [move_dic , click_dic]
             pickle.dump(data_to_store , data)
             data.close()
-            heatmap.save("heatmap.png")
-
-
+            heatmap_click.save("heatmap_click.png")
+            heatmap_move.save("heatmap_move.png")
+    print("Data stored")
 with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
     listener.join()
-    
